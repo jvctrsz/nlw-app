6c8951ddcb231ac44ffc4c9f355465e3e7f6899f
João Victor Matias <108106191+jvctrsz@users.noreply.github.com>
2023-01-19 19:34:03 -0400

    js library

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const form = document.querySelector('form')
 const nlwSetup = new NLWSetup(form)
 
+const data = {
+    workout: ['01-01', '01-02','01-03'],
+    water: ['01-01', '01-03','01-06'],
+    fruit: ['01-01', '01-0','01-05']
+}
